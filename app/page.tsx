1a3628ba817d7084f7572a2d53fadf686e0f8a0f
Nikhilesh <98324253+justnikhilesh@users.noreply.github.com>
2024-02-17 15:01:59 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="container">
+    <h1>
+      hello there
+      this is my first page
+    </h1>
+    <br></br>
+    <hr />
+    <div className="text-5xl">helooo</div>
+    </div>
+  );
+}
